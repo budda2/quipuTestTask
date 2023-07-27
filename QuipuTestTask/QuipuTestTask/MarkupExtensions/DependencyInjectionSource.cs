3189f8b427c6109d67035a8768dba1022b211e4c
Mykhailo Haidei <popuhailo@gmail.com>
2023-07-27 21:45:00 +0300

    Added markup extension for view dependency injection

---
+++ QuipuTestTask/QuipuTestTask/MarkupExtensions/DependencyInjectionSource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace QuipuTestTask.MarkupExtensions;
+
+public class DependencyInjectionSource : MarkupExtension
+{
+    public Type Type { get; set; }
+
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
+        serviceProvider.GetService(Type) ?? throw new Exception($"Dependency of type {Type} is not registered");
+}
